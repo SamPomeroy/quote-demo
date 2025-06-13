19ba01d89f388c6b0e881508f9a48203285b6e51
SamPomeroy <samanthapomeroy38@gmail.com>
2025-06-13 13:37:02 -0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/quote',
+        destination: 'http://localhost:5678/webhook/quote'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig;
